63ce83b2371b06451047846a1929f7daf9c90727
Daren Welsh <darenwelsh@gmail.com>
2015-12-05 18:21:36 -0600

    restructure using functions

--- parse.php
+++ parse.php
@@ -5,52 +5,13 @@
 # this will require authentication
 # $file =  file_get_contents('https://mod2.jsc.nasa.gov/dm/DM33/TOPO_WEB/traj_data/stp/topo52.ISS.sun_lighting_events.txt');
 
-date_default_timezone_set('UTC');
+function renderDayNightRow( array $timeEvent, $inputTime, $timelineDuration ) {
+	// These two variables are the first and last timestamps for data available via TOPO
+	# TO-DO: Tell user range of dates which are eligible to use web data (in form)
+	# TO-DO: Allow user to opt out of using web data if in that range
+	$availableDataFirstTimestamp = $timeEvent[0]['timestamp'];
+	$availableDataLastTimestamp  = $timeEvent[count($timeEvent)-1]['timestamp'];
 
-$file = file_get_contents('topo52.ISS.sun_lighting_events.txt');
-$rows = explode("\n", $file);
-
-// Parse rows
-foreach($rows as $row => $data){
-	// split the data elements by whitespace
-	$rowData[$row] = preg_split("/[\s]+/", $data);
-
-	// if row has >10 elements and starts with numbers, then it is likely real data and not a header
-	// also only keep rows where 'event' == Start_Sunrise or Full_Sunset
-	if( count($rowData[$row]) > 10
-	&& preg_match("/^[0-9]/", $rowData[$row][1])
-	&& ( $rowData[$row][10] == "Start_Sunrise" || $rowData[$row][10] == "Full_Sunset" ) ){
-		$timeEvent[$row]['timestamp'] = $rowData[$row][1];
-		$timeEvent[$row]['event']     = $rowData[$row][10];
-
-		// reformat timestamp
-		$tempTimeArray = preg_split("/:/", $timeEvent[$row]['timestamp']);
-		$tempYear   = $tempTimeArray[0];
-		$tempMonth  = $tempTimeArray[1];
-		$tempDay    = $tempTimeArray[2];
-		$tempHour   = $tempTimeArray[3];
-		$tempMinute = $tempTimeArray[4];
-		$timeEvent[$row]['timestamp'] = strtotime($tempYear . "-" . $tempMonth . "-" . $tempDay . " " . $tempHour . ":" . $tempMinute);
-	}
-
-}
-
-// Remove all the rows (keys) we don't want
-$timeEvent = array_values($timeEvent);
-
-$inputTime = strtotime($argv[1]); // "YYYY-MM-DD HH:MM" when PET = 000
-$timelineDuration = $argv[2]; // Timeline duration in minutes
-
-// These two variables are the first and last timestamps for data available via TOPO
-# TO-DO: Tell user range of dates which are eligible to use web data (in form)
-# TO-DO: Allow user to opt out of using web data if in that range
-$availableDataFirstTimestamp = $timeEvent[0]['timestamp'];
-$availableDataLastTimestamp  = $timeEvent[count($timeEvent)-1]['timestamp'];
-
-$canUseData = "no"; // Used by Summary Timeline generation
-// Determine if $inputTime exists within the available data (between first row and last row)
-if( ($timeEvent[0]['timestamp'] < $inputTime) && (($inputTime + (60 * $timelineDuration)) < $timeEvent[count($timeEvent)-1]['timestamp']) ){
-	$canUseData = "yes"; // Used by Summary Timeline generation
 	// Determine which row to use as first period (when PET = 000)
 	// Start at first row of data and parse through until the input time passes the row time
 	$timeIndex = 0;
@@ -193,12 +154,70 @@ if( ($timeEvent[0]['timestamp'] < $inputTime) && (($inputTime + (60 * $timelineD
 
 	print_r("\n\n=========================\n\n");
 
+}
+
+function getData(){
+	$file = file_get_contents('topo52.ISS.sun_lighting_events.txt');
+	$rows = explode("\n", $file);
+
+	// Parse rows
+	foreach($rows as $row => $data){
+		// split the data elements by whitespace
+		$rowData[$row] = preg_split("/[\s]+/", $data);
+
+		// if row has >10 elements and starts with numbers, then it is likely real data and not a header
+		// also only keep rows where 'event' == Start_Sunrise or Full_Sunset
+		if( count($rowData[$row]) > 10
+		&& preg_match("/^[0-9]/", $rowData[$row][1])
+		&& ( $rowData[$row][10] == "Start_Sunrise" || $rowData[$row][10] == "Full_Sunset" ) ){
+			$timeEvent[$row]['timestamp'] = $rowData[$row][1];
+			$timeEvent[$row]['event']     = $rowData[$row][10];
+
+			// reformat timestamp
+			$tempTimeArray = preg_split("/:/", $timeEvent[$row]['timestamp']);
+			$tempYear   = $tempTimeArray[0];
+			$tempMonth  = $tempTimeArray[1];
+			$tempDay    = $tempTimeArray[2];
+			$tempHour   = $tempTimeArray[3];
+			$tempMinute = $tempTimeArray[4];
+			$timeEvent[$row]['timestamp'] = strtotime($tempYear . "-" . $tempMonth . "-" . $tempDay . " " . $tempHour . ":" . $tempMinute);
+		}
+
+	}
+
+	// Remove all the rows (keys) we don't want
+	$timeEvent = array_values($timeEvent);
+
+	return $timeEvent;
+
+}
+
+
+date_default_timezone_set('UTC');
+$inputTime = strtotime($argv[1]); // "YYYY-MM-DD HH:MM" when PET = 000
+$timelineDuration = $argv[2]; // Timeline duration in minutes
+
+$timeEvent = getData();
+
+// These two variables are the first and last timestamps for data available via TOPO
+# TO-DO: Tell user range of dates which are eligible to use web data (in form)
+# TO-DO: Allow user to opt out of using web data if in that range
+$availableDataFirstTimestamp = $timeEvent[0]['timestamp'];
+$availableDataLastTimestamp  = $timeEvent[count($timeEvent)-1]['timestamp'];
+
+$canUseData = "no"; // Used by Summary Timeline generation
+// Determine if $inputTime exists within the available data (between first row and last row)
+if( ($timeEvent[0]['timestamp'] < $inputTime) && (($inputTime + (60 * $timelineDuration)) < $timeEvent[count($timeEvent)-1]['timestamp']) ){
+	renderDayNightRow($timeEvent, $inputTime, $timelineDuration);
+
+	$canUseData = "yes"; // Used by Summary Timeline generation
 
 } else {
 	// We can't use this data from TOPO
 	// So we'll have to use data from user
-	// print_r("No");
+
 }
+
 
 
 
